fcf436d282614689c9c58719e755a811437ec10e
Koki Sakai <likuor0528@gmail.com>
2022-08-26 13:55:37 -0700

    Add firebase config

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+// import { signInWithPopup } from 'firebase/auth';
+// import { auth, provider } from '../firebase/FirebaseConfig';
+
+const loginGoogle = () => {
+  console.log('herer');
+  // signInWithPopup(auth, provider);
+};
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <button onClick={loginGoogle}>Login</button>
+          </li>
+          <li>
+            <button>Logout</button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
