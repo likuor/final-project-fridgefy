7377e02312b0687529d1189b49c962b3e9df5e0e
Koki Sakai <likuor0528@gmail.com>
2022-08-30 11:28:38 -0700

    Add parameter to add addData function

--- src/helper/AddDataToFirebase.jsx
+++ src/helper/AddDataToFirebase.jsx
@@ -6,22 +6,49 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 const AddDataToFirebase = () => {
   const [user] = useAuthState(auth);
 
-  const addData = () => {
-    if (user) {
-      const getCollection = doc(collection(database, 'fridge'));
+  const addData = (collectionName) => {
+    const getCollection = doc(collection(database, collectionName));
+    if (user && collectionName === 'fridge') {
       setDoc(getCollection, {
-        id: 3,
-        image: 'test3',
-        name: 'test3',
+        id: 4,
+        image: 'test4',
+        name: 'test4',
         userId: user.uid,
       });
-      console.log('Added');
+      console.log('Added ingredients');
+    } else if (user && collectionName === 'recipe') {
+      setDoc(getCollection, {
+        id: 4,
+        image: 'test4',
+        information: 'test4',
+        ingredients: ['sea weed', 'rice'],
+        name: 'sushi',
+        userId: user.uid,
+      });
+      console.log('Added recipe');
     } else {
-      alert('Need to login to add ingredients');
+      alert('Need to login to add a recipe');
     }
   };
 
-  return <button onClick={addData}>AddDataToFirebase</button>;
+  return (
+    <>
+      <button
+        onClick={() => {
+          addData('fridge');
+        }}
+      >
+        Ingredients
+      </button>
+      <button
+        onClick={() => {
+          addData('recipe');
+        }}
+      >
+        Recipe
+      </button>
+    </>
+  );
 };
 
 export default AddDataToFirebase;
