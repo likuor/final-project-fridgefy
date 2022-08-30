3aa6f3079a17dbe9713e7af9205170fdeb8279cc
asafebxd <alves.asafecabral@gmail.com>
2022-08-30 09:11:34 -0700

    items to buy file

---
+++ src/pages/ItemsToBuy.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+const  ItemsToBuy = () => {
+    return (
+        <div>
+            <div>Items to Buy </div>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>        
+        </div>
+    )
+}
+
+export default ItemsToBuy
