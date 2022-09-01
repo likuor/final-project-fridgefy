d33a56b17e8d2036824646c71278a0f9731d09d4
Koki Sakai <likuor0528@gmail.com>
2022-09-01 10:57:21 -0700

    Merged from Tomo

--- src/pages/IngredientsList.jsx
+++ src/pages/IngredientsList.jsx
@@ -1,19 +1,20 @@
-import React, { useEffect, useRef, useState, useContext } from "react";
-import { collection, addDoc, deleteDoc, doc } from "firebase/firestore";
-import axios from "axios";
-import styled from "styled-components";
-import ShowDataFromFirebase from "../helper/ShowDataFromFirebase";
-import { database, auth } from "../firebase/FirebaseConfig";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { IngredientsDataContext } from "./IngredientsDataContext";
+import React, { useEffect, useRef, useState, useContext } from 'react';
+import { collection, addDoc, deleteDoc, doc } from 'firebase/firestore';
+import axios from 'axios';
+import styled from 'styled-components';
+import ShowDataFromFirebase from '../helper/ShowDataFromFirebase';
+import { database, auth } from '../firebase/FirebaseConfig';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { IngredientsDataContext } from './IngredientsDataContext';
 
 export default function IngredientsList() {
   const inputRef = useRef(null);
   const { dispatch } = useContext(IngredientsDataContext);
   const { state } = useContext(IngredientsDataContext);
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState('');
   const [searchIngredientsArray, setSearchIngredientsArray] = useState([]);
   const [userIngredientsArray, setUserIngredientsArray] = useState([]);
+  const [user] = useAuthState(auth);
 
   const IngredientsList = {
     get: async (ingredients) => {
@@ -24,10 +25,7 @@ export default function IngredientsList() {
     },
   };
 
-  const [user] = useAuthState(auth);
-
-  ShowDataFromFirebase("Ingredients", setUserIngredientsArray);
-
+  ShowDataFromFirebase('Ingredients', setUserIngredientsArray);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -43,16 +41,15 @@ export default function IngredientsList() {
   };
 
   const onClickIngredients = async (data) => {
-    const nameCollection = collection(database, "Ingredients");
+    const nameCollection = collection(database, 'Ingredients');
     const newIngredient = {
-
       image: `${data.name}.jpg`,
       name: data.name,
       userId: user.uid,
     };
 
     const documentIngredients = await addDoc(nameCollection, newIngredient);
-    console.log("doc added", documentIngredients);
+    console.log('doc added', documentIngredients);
     setSearchIngredientsArray([]);
     setUserIngredientsArray([
       ...userIngredientsArray,
@@ -61,16 +58,16 @@ export default function IngredientsList() {
   };
 
   const deleteItem = async (item) => {
-    console.log("item", item);
-    console.log("check item", userIngredientsArray);
+    console.log('item', item);
+    console.log('check item', userIngredientsArray);
     try {
-      await deleteDoc(doc(database, "Ingredients", item.dbId));
+      await deleteDoc(doc(database, 'Ingredients', item.dbId));
       const filteredArray = userIngredientsArray.filter(
         (itemList) => itemList.dbId !== item.dbId
       );
       setUserIngredientsArray(filteredArray);
     } catch (err) {
-      console.log("err", err);
+      console.log('err', err);
     }
   };
 
@@ -79,10 +76,10 @@ export default function IngredientsList() {
       {
         return array.map((item, index) => {
           return (
-            <div className="ingredients_list_container">
+            <div className='ingredients_list_container' key={index}>
               <h2>{item.name}</h2>
               <button
-                className="ingredients_list_button"
+                className='ingredients_list_button'
                 onClick={() => deleteItem(item)}
               >
                 X
@@ -117,13 +114,13 @@ export default function IngredientsList() {
       <div>
         <h2>My Fridge</h2>
         <form onSubmit={handleSubmit}>
-          <input name="name" type="text" placeholder="Name" ref={inputRef} />
+          <input name='name' type='text' placeholder='Name' ref={inputRef} />
           <button>Search</button>
         </form>
-        <div className="ingredientsList_container">
+        <div className='ingredientsList_container'>
           {userFavoriteIngredientsList(searchIngredientsArray)}
         </div>
-        <div className="ingredients_list">
+        <div className='ingredients_list'>
           {userIngredientsDataList(userIngredientsArray)}
         </div>
       </div>
@@ -132,23 +129,23 @@ export default function IngredientsList() {
 }
 
 const StyleIngredientsContainer = styled.div`
-    .ingredientsList_container {
-      background-color: aqua;
-      position: absolute;
-      top: 6rem;
-      left: 15rem;
-      width: 10rem;
-    }
+  .ingredientsList_container {
+    background-color: aqua;
+    position: absolute;
+    top: 6rem;
+    left: 15rem;
+    width: 10rem;
+  }
 
-    .ingredients_list {
-      position: relative:
-    }
+  .ingredients_list {
+    position: relative;
+  }
 
-    .ingredients_list_container {
-        display: flex;
-    }
-    .ingredients_list_button {
-        margin-left: 3rem;
-        width: 2rem;
-    }
-  `;
+  .ingredients_list_container {
+    display: flex;
+  }
+  .ingredients_list_button {
+    margin-left: 3rem;
+    width: 2rem;
+  }
+`;
