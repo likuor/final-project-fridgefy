4379f033e699964503ba7768289aae2c0b523c03
helenavprado <helena.vprado@hotmail.com>
2022-09-01 15:10:20 -0700

    componets
    Did the styling for: Navbar, HeroPage, AllRecipes, MyRecipes

--- src/pages/HeroPage.jsx
+++ src/pages/HeroPage.jsx
@@ -1,34 +1,88 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import AuthFirebase from "../firebase/AuthFirebase";
+import img from "../images/heroPageBackground.jpg"
 
 export default function HeroPage() {
   return (
     <StyleHeroPage>
-      <div className="login_part">
-        <div className="login-container">
-          <div id="logo" className="font">
-            Fridgefy
-          </div>
-          <div className="username-container">
-            <button className="styled-button" id="hero-page-button">
-              Sign In
-            </button>
-          </div>
-          <div className="font">or</div>
-          <div className="guest">continue as guest</div>
-          <div>
-            <Link to={`/recipesPage/`}>Go to the page</Link>
-          </div>
+      <body class="hero-page">
+        <div class="login-container">
+            <div id="logo" class="font">Fridgefy</div>
+            <div class="username-container">
+                <AuthFirebase class="styled-button" id="hero-page-button"/>
+            </div>
+            <div class="font">or</div>
+            <Link to={`/recipesPage`} class="guest">continue as a guest</Link>
         </div>
-      </div>
+      </body>
     </StyleHeroPage>
   );
 }
 
 const StyleHeroPage = styled.div`
-  .login_part {
-    display: flex;
-    justify-content: center;
-  }
+@import url('https://fonts.googleapis.com/css2?family=Pacifico&family=Yanone+Kaffeesatz&display=swap');
+.hero-page{
+  height: 65vw;
+  background: url(${img}) no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+
+.login-container{
+  height 45%;
+  width: 50%;
+  background-color: rgba(255, 255, 255, 0.9);
+  border-radius: 10px;
+  position: relative;
+  top: 100px;
+  margin: 0 auto;
+  box-shadow: 1px 1px 5px 5px #63b8a034;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.username-container {
+  margin: 40px auto;
+}
+
+.username-container > input {
+  border: #63b8a07a solid 1px;
+}
+
+#logo {
+  font-size: 50px;
+  height: fit-content;
+  width: fit-content;
+}
+
+.font {
+  color: #63B8A0;
+  font-family: 'Pacifico', cursive;
+  font-size: 30px;
+}
+
+.guest {
+  text-decoration: underline;
+  color: #559c88;
+  font-family: 'Yanone Kaffeesatz', sans-serif;
+  font-size: 20px;
+}
+
+#hero-page-button{
+  height: 50px;
+  width: 100px;
+  font-size: 25px;
+  font-family: 'Yanone Kaffeesatz', sans-serif;
+  letter-spacing: 1px;
+}
+
+.guest:hover{
+  cursor: pointer;
+}
 `;
--- src/pages/MyRecipes.jsx
+++ src/pages/MyRecipes.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export default function MyRecipes(props) {
   console.log("fridge", props.fridge);
@@ -8,22 +10,69 @@ export default function MyRecipes(props) {
   // ShowDataFromFirebase("recipe", props.setRecipe);
 
   return (
-    <div>
-    //   <button>My Recipes</button>
-      <div>
-    //<ul>
-        {props.recipe.map((data, index) => {
-            return (
-            <div key={index}>
-              <li>{data.name}
-                    {data.image}
-              </li>
-              <button>X</button>
+  <StyleLeftBar>
+        <div class="container">
+          <div class="sides_container" id="left-bar">
+            <div class="button-container">
+              <Link to={`/recipesPage`}><button class="styled-button">My Recipes</button></Link>
+              <Link to={`/shoppingListPage`}><button class="styled-button">Shopping List</button></Link>
             </div>
-          );
-        })}
-      </ul>     
-      </div>
-    </div>
+            <div>
+              <ul>
+                  {props.recipe.map((data, index) => {
+                      return (
+                      <div key={index}>
+                        <li>{data.name}
+                              {data.image}
+                        </li>
+                        <button>X</button>
+                      </div>
+                    );
+                  })}
+              </ul>     
+            </div>
+          </div>
+        </div>
+  </StyleLeftBar>
   );
 }
+
+const StyleLeftBar = styled.div`
+@import url('https://fonts.googleapis.com/css2?family=Pacifico&family=Yanone+Kaffeesatz&display=swap');
+
+.container {
+  margin-top: 15px;
+  font-family: 'Yanone Kaffeesatz', sans-serif;
+  display: flex;
+}
+
+.sides_container {
+  background-color: #7EEBCC;
+  width: 20vw;
+  height: 50vw;
+  border-radius: 10px;
+  padding: 15px;
+  color: whitesmoke;
+  font-size: 25px;
+}
+
+.styled-button {
+  border-radius: 5px;
+  border: none;
+  height: fit-content;
+  width: fit-content;
+  padding: 5px 15px;
+  background-color: #63B8A0;
+  color: whitesmoke;
+}
+
+.styled-button:hover{
+  background-color: #559c88;
+  cursor: pointer;
+}
+
+.button-container{
+  display: flex;
+  justify-content: space-around;
+}
+`;
